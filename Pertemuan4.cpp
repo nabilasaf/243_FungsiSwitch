442fe62a81ce4e0a9131e04f61a12462d5aa2daf
nabilasaf <nabilasafitri861@gmail.com>
2024-03-26 15:07:32 +0700

    memasukkan_data

--- Pertemuan4.cpp
+++ Pertemuan4.cpp
@@ -5,6 +5,14 @@
 using namespace std;
 int bilangan1, bilangan2;
 
+void inputData()
+{
+    cout << "Masukan bilangan pertama : ";
+    cin >> bilangan1;
+    cout << "Masukan bilngan kedua : ";
+    cin >> bilangan2;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
